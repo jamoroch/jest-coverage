242cc68a6d5991986b9cf2ad3538f4862878589f
jamoroch <jamoroch@gmail.com>
2021-05-01 20:51:20 +0200

    add html report to jest

--- jest.config.js
+++ jest.config.js
@@ -1,8 +1,15 @@
 module.exports = {
   preset: 'ts-jest',
   testEnvironment: 'node',
+  reporters: [
+    'default',
+    ['./node_modules/jest-html-reporter', {
+		'pageTitle': 'Test Result',
+                'outputPath': 'test_results/index.html'
+	}]
+  ],
   coverageReporters: [
-       "text-summary",
-       "html"
+       'text-summary',
+       'html'
   ],
 };
