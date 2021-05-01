56256797c3cc422d86bf5360448bd1616d29ec3a
jamoroch <jamoroch@gmail.com>
2021-05-01 20:55:18 +0200

    add css

--- jest.config.js
+++ jest.config.js
@@ -5,7 +5,8 @@ module.exports = {
     'default',
     ['./node_modules/jest-html-reporter', {
 		'pageTitle': 'Test Result',
-                'outputPath': 'test_results/index.html'
+                'outputPath': 'test_results/index.html',
+                'useCssFile': true
 	}]
   ],
   coverageReporters: [
